7c5ddf8697401fd1d10af11393ca263c5eb9c228
Aurembiaix7 <aurembiaixsm27@gmail.com>
2024-12-23 19:59:00 +0100

    Revisió

--- Quantum-Well-Simulation/Quantum_Well_Simulation.py
+++ Quantum-Well-Simulation/Quantum_Well_Simulation.py
@@ -12,16 +12,16 @@ width=60. # Well Width [Amg]
 # Setting up Hamiltonian Matrix
 H=np.zero((nombrePunts,nombrePunts))
 offdiag=-hbar2om(2*delta**2*meff)
-for i in range (nombrePunts-2): # Offdiagonal Hamiltonian elements
+for i in range: # Offdiagonal Hamiltonian elements
     H[i,i+1]=offdiag
     H[i,i-1]=offdiag
-for i in range(nombrePunts-1): # Anirà de 0 anombrePunts-1
+for i in range: # Anirà de 0 anombrePunts-1
     H[i,i]=-2.*offdiag
 # Afegim el terme de la barrera
 nwidth=width/delta # Quants punts tindrà la barrera
 half=nombrePunts//2 # Divisió entera per nombrePunts imparell
-for i in range(nombrePunts-1): # Anirà de 0 anombrePunts-1
-if((i<half-nwidth//2)or(i>half+nwidth//2)):
+for i in range: # Anirà de 0 anombrePunts-1
+if (i<half-nwidth//2) or (i>half+nwidth//2)
     H[i,i]+=VB # Afegir la Valence Band
 
 # Diagonalise Hamiltioian Matrix
